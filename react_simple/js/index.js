5bb58b7caec015ba7248a3474a77d871a231a594
Chacha Sikes <chachasikes@gmail.com>
2018-04-23 09:25:24 -0700

    react-basics example

--- react_simple/js/index.js
+++ react_simple/js/index.js
@@ -1,9 +1,97 @@
-// Import not needed because React & ReactDOM are loaded by *.libraries.yml
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+class ToggleButton extends React.Component {
+  constructor() {
+    super();
+    this.state = { status: 0 };
+    this.buttonActivate = this.buttonActivate.bind(this);
+    this.buttonDisable = this.buttonDisable.bind(this);
+  }
+  buttonActivate() {
+    this.setState({status: 1});
+  }
+  buttonDisable() {
+    this.setState({status: 0});
+  }
+  render() {
+    var style = {
+      active: {
+        width: '200px',
+        height: '40px',
+        padding: '10px',
+        margin: '10px',
+        backgroundColor: '#fff',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignSelf: 'center',
+      },
+      inactive: {
+        width: '200px',
+        height: '40px',
+        padding: '10px',
+        margin: '10px',
+        backgroundColor: '#cccccc',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignSelf: 'center',
+      }
+    };
+    return (
+      <div>
+        {this.state.status === 1 &&
+          <div
+            onClick={this.buttonDisable}
+            style={style.active}
+          >
+            Enabled
+          </div>
+        }
+        {this.state.status === 0 &&
+          <div
+            onClick={this.buttonActivate}
+            style={style.inactive}
+          >
+            Disabled
+          </div>
+        }
+      </div>
+    );
+  }
+}
 
-// # Example 1: Simple Hello World code
+// # Example 2
+// Create a new component that displays the content of the node.
+class Welcome extends React.Component {
+  // Set up the initial state.
+  constructor() {
+    super();
+    this.state = { name: 'World' };
+  }
+  // Output
+  render() {
+    var style = {
+      color: '#fff',
+      backgroundColor: '#DDDDDD',
+    };
+    // View data in console.
+    // console.log(this.props.children);
+    return (
+      <div
+        style={style}
+      >
+        <h1>Hello, {this.state.name}. {this.props.message}!</h1>
+        <ToggleButton />
+        {/* Raw markup */}
+        {/* {this.props.children.join('')} */}
+
+        {/* Put node content in a div & render HTML */}
+        <div dangerouslySetInnerHTML={{ __html: this.props.children.join('') }} />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello there - world!</h1>,
+  <Welcome message="Hi! How are things?"> { document.getElementById('react-app').innerHTML } </Welcome>,
   document.getElementById('react-app')
 );
