7a669ec4442f92d6830f402e8a480708a1edacaa
Chacha Sikes <chachasikes@gmail.com>
2018-04-23 13:41:55 -0700

    setting up show more

--- react_simple/js/index.js
+++ react_simple/js/index.js
@@ -1,3 +1,64 @@
+class ShowNodeContent extends React.Component {
+
+  render() {
+    var style = {
+      active: {
+        width: '200px',
+        height: '40px',
+        padding: '10px',
+        margin: '10px',
+        color: '#333',
+        backgroundColor: '#ddd',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignSelf: 'center',
+      },
+      inactive: {
+        width: '200px',
+        height: '40px',
+        padding: '10px',
+        margin: '10px',
+        color: '#666',
+        backgroundColor: '#ccc',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignSelf: 'center',
+      },
+      content: {
+        margin: '30px',
+        padding: '30px',
+        backgroundColor: '#eee',
+        border: '5px solid black'
+      }
+    };
+
+    return (
+      <div>
+          <div>
+            <div
+              onClick={this.buttonDisable}
+              style={style.active}
+            >
+              Hide content
+            </div>
+            <div style={style.content}>
+              {this.props.content}
+            </div>
+          </div>
+
+          <div
+            onClick={this.buttonActivate}
+            style={style.inactive}
+          >
+            Show more
+          </div>
+      </div>
+    );
+  }
+}
+
 // Create a new component that displays the content of the node.
 class NodeContent extends React.Component {
   render() {
