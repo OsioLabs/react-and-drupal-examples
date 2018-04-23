e387f68ceb8579a3381139a58565923df7bd7ca5
Chacha Sikes <chachasikes@gmail.com>
2018-04-23 11:58:52 -0700

    work on updates to node-list

--- react_simple/js/index.js
+++ react_simple/js/index.js
@@ -1,149 +1,109 @@
-class ShowNodeContent extends React.Component {
+class NodeItem extends React.Component {
+  render() {
+    return <div>
+      <h2>{this.props.attributes.title}</h2>
+      <div dangerouslySetInnerHTML={{__html: this.props.attributes.body.value}} />
+    </div>;
+  }
+}
+
+class NoData extends React.Component {
+  render() {
+    return <div>No articles found.</div>;
+  }
+}
+
+class Loading extends React.Component {
+  render() {
+    return <div>Loading data</div>;
+  }
+}
+
+class NodeList extends React.Component {
   constructor() {
     super();
-    this.state = {
-      status: 0,
-      typeEffectContent: null,
-    };
-    this.buttonActivate = this.buttonActivate.bind(this);
-    this.buttonDisable = this.buttonDisable.bind(this);
+    this.state = { data: null };
+    this.loadNodeData = this.loadNodeData.bind(this);
+    this.updateData = this.updateData.bind(this);
+    this.checkInvalidData = this.checkInvalidData.bind(this);
   }
-  componentDidMount() {
-    let { content } = this.props;
-    if (content !== undefined &&
-      content !== null) {
-      var lines = [];
-      var maxLineLength = 100;
-      var words = content.split(" ");
-      var currentLine = words[0];
-      var lineLength = currentLine.length;
-      for (var i = 1; i < words.length; i++) {
-          var word = words[i];
-          lineLength += word.length + 1;
-          if (word !== "") {
-            if (lineLength <= maxLineLength) {
-              currentLine += " " + word;
-            } else {
-              lines.push(currentLine);
-              currentLine = word;
-              lineLength = word.length;
-            }
-          }
-        }
-        lines.push(currentLine);
-        this.setState({typeEffectContent: lines });
+
+  componentWillMount() {
+    this.loadNodeData();
+  }
+
+  loadNodeData() {
+    const API_ROOT = 'http://localhost:8888/rd/jsonapi/';
+    const url = `${API_ROOT}node/article`;
+
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then((data) => this.updateData(data))
+      .catch(err => console.log('API got an error', err))
+  }
+
+  updateData(responseData) {
+    console.log(responseData);
+    const validatedData = this.checkInvalidData(responseData);
+    if (validatedData) {
+      this.setState( { data: responseData.data }, () => console.log(this.state));
     }
   }
 
-  componentDidUpdate() {
-    if (this.state.status === 1 &&
-        this.state.typeEffectContent !== null) {
-        // https://mattboldt.com/demos/typed-js/
-        // https://github.com/mattboldt/typed.js/blob/master/README.md
-        // Full docs: https://mattboldt.com/typed.js/docs/
-        var typed = new Typed('#typed', {
-          strings: this.state.typeEffectContent,
-          typeSpeed: 0,
-          backSpeed: 0,
-          fadeOut: true,
-          loop: false
-        });
+  checkInvalidData(data) {
+    if (data === null) {
+      return false;
     }
+    if (data.data === undefined ||
+        data.data === null ||
+        data.data.length === 0 ) {
+      return false;
+    }
+    return true;
   }
-  buttonActivate() {
-    this.setState({status: 1});
-  }
-  buttonDisable() {
-    this.setState({status: 0});
-  }
+
   render() {
-    var style = {
-      active: {
-        width: '200px',
-        height: '40px',
-        padding: '10px',
+    const style = {
+      nodeItem: {
+        border: '2px solid black',
         margin: '10px',
-        color: '#333',
-        backgroundColor: '#ddd',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignSelf: 'center',
-      },
-      inactive: {
-        width: '200px',
-        height: '40px',
         padding: '10px',
-        margin: '10px',
-        color: '#666',
         backgroundColor: '#ccc',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignSelf: 'center',
       },
-      content: {
-        margin: '30px',
-        padding: '30px',
-        backgroundColor: '#eee',
-        border: '5px solid black'
+      nodeItemActive: {
+        border: '2px solid black',
+        margin: '10px',
+        padding: '10px',
+        backgroundColor: '#fff',
       }
     };
 
     return (
       <div>
-        {this.state.status === 1 &&
-          <div>
-            <div
-              onClick={this.buttonDisable}
-              style={style.active}
-            >
-              Hide content
-            </div>
-            <div style={style.content}>
-              <div id="typed" />
-            </div>
-          </div>
-        }
-        {this.state.status === 0 &&
-          <div
-            onClick={this.buttonActivate}
-            style={style.inactive}
-          >
-            Show more
-          </div>
-        }
-      </div>
-    );
-  }
-}
+        <h2>Other content</h2>
 
-// # Example 2
-// Create a new component that displays the content of the node.
-class NodeContent extends React.Component {
-  // Set up the initial state.
-  constructor() {
-    super();
-    this.state = { example: 'An example of a state variable' };
-  }
-  // Output
-  render() {
-    // View data in console.
-    // console.log(this.props.children);
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <ShowNodeContent content={this.props.content}>
-          <h2>{this.state.example}</h2>
-          {/* Put node content in a div & render it as HTML */}
-          <div dangerouslySetInnerHTML={{ __html: this.props.content }} />
-        </ShowNodeContent>
+        {this.state.data !== null ?
+          this.state.data.map(item => {
+            let className = 'nodeItem';
+            console.log(item);
+            console.log(this.props.nid);
+            if (this.props.nid === item.attributes.nid) {
+              className = 'nodeItemActive';
+            }
+
+            return (<div style={style[className]}> <NodeItem {...item} /> </div>);
+          } )
+          :
+          <NoData />
+        }
       </div>
     );
   }
 }
 
 ReactDOM.render(
-  <NodeContent title={document.getElementById('react-app').getAttribute("data-title")} content={document.getElementById('react-content').textContent} />,
+  <NodeList title={document.getElementById('react-app').getAttribute("data-title")} nid={document.getElementById('react-app').getAttribute('data-nid')} />,
   document.getElementById('react-app')
 );
