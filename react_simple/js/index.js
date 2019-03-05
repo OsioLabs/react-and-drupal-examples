abbb638b4f28e755a142d2bd6407e9135e2c35d3
Joe Shindelar <eojthebrave@gmail.com>
2019-03-05 15:15:37 -0600

    Update NodeItem to verify body attribute exists before displaying it.

--- react_simple/js/index.js
+++ react_simple/js/index.js
@@ -2,7 +2,7 @@ class NodeItem extends React.Component {
   render() {
     return <div>
       <h2>{this.props.attributes.title}</h2>
-      <div dangerouslySetInnerHTML={{__html: this.props.attributes.body.value}} />
+      {this.props.attributes.body && <div dangerouslySetInnerHTML={{__html: this.props.attributes.body.value}} />}
     </div>;
   }
 }
